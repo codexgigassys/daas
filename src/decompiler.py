86e42f538df196394eb6ab535f26b6a92ecca797
lucasesposito <espositolucas95@gmail.com>
2018-08-24 20:07:22 -0300

    better decompilation function

--- src/decompiler.py
+++ src/decompiler.py
@@ -1,36 +1,48 @@
 import subprocess
 import logging
+import time
 subprocess_function = "subprocess.check_output(@command, stderr=subprocess.STDOUT)"
 TIMEOUT = 1000  # Seconds
 DOCUMENT_PATH = ''
-EXTRACTION_DIRECTORY= ''
+EXTRACTION_DIRECTORY = ''
 
 
-# Example:
-# timedout_subprocess_eval('subprocess.check_output(@command, stderr=subprocess.STDOUT)',
-#                          command = ["echo", "i\'m a command!"])
-def timedout_subprocess_eval(self, subprocess_function_as_string, bash_command):
+def csharp_decompile():
     nice_command = ['nice', '-n', '2']
     timeout_command = ['timeout', '-k', '30', str(TIMEOUT)]
     csharp_command = ["wine", "/just_decompile/ConsoleRunner.exe", "/target:" + DOCUMENT_PATH, "/out:" + EXTRACTION_DIRECTORY]
     full_command = nice_command + timeout_command + csharp_command
-    info_for_statistics = {'timeout': TIMEOUT}
     logging.debug('Running command: %s' % full_command)
+    start = time.time()
     try:
         start = time.time()
-        output = eval(full_subprocess_function)
+        output = subprocess.check_output(full_command, stderr=subprocess.STDOUT)
+        # Details and info for statistics
         elapsed_time = time.time() - start
-        self.info_for_statistics['elapsed_time'] = int(elapsed_time) + 1
-        self.info_for_statistics['exit_status'] = 0
+        exit_status = 0  # The decompiler didn't crash
+        exception_info = {}
     except subprocess.CalledProcessError as e:
+        # Details and info for statistics
         elapsed_time = time.time() - start
-        info_for_statistics['elapsed_time'] = int(elapsed_time) + 1
-        info_for_statistics['exit_status'] = e.returncode
-        logging.debug('Subprocess raised CalledProcessError exception. Duration: %s seconds. Timeout: %s seconds' % (int(elapsed_time), int(self.getTimeout())))
-        logging.debug('Exception info: %s' % {'return_code': e.returncode, 'output': e.output, 'message': e.message, 'command': e.cmd, 'arguments': e.args})
-        if e.returncode == 124:
-            logging.debug('Process timed out. Raising exception...')
+        exit_status = e.returncode
+        exception_info = {'output': e.output,
+                          'message': e.message,
+                          'command': e.cmd,
+                          'arguments': e.args}
+        logging.debug('Subprocess raised CalledProcessError exception. Duration: %s seconds. Timeout: %s seconds' % (int(elapsed_time), TIMEOUT))
+        logging.debug('Exception info: %s' % exception_info)
+        # Exception handling
+        if exit_status == 124:  # exit status is 124 when the timeout is reached.
+            logging.debug('Process timed out.')
         else:
-            logging.debug('Strange status code: %s. Raising exception...' % e.returncode)
-        raise e
-    return self.decode_output(output)
+            logging.debug('Unknown exit status code: %s.' % exit_status)
+    info_for_statistics = {'timeout': TIMEOUT,
+                           'elapsed_time': int(elapsed_time) + 1,
+                           'exit_status': exit_status,
+                           'exception_info': exception_info}
+    return decode_output(output), info_for_statistics
+
+
+def decode_output(output):
+    """ TODO """
+    return output
