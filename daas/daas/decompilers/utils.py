75763e76d4e12d284ae2b64e2d4a5db1d5cbd2ec
lucasesposito <espositolucas95@gmail.com>
2018-09-08 10:53:42 -0300

    added class to register relation between conditions and decompilers

--- daas/daas/decompilers/utils.py
+++ daas/daas/decompilers/utils.py
@@ -5,3 +5,31 @@ class Singleton(object):
         if not isinstance(class_._instance, class_):
             class_._instance = object.__new__(class_, *args, **kwargs)
         return class_._instance
+
+
+class Relation:
+    def __init__(self, filter, queue, worker):
+        self.filter = filter
+        self.queue = queue
+        self.worker = worker
+
+    def send_to_queue_if_necessary(self, sample):
+        """ Sends the sample to the queue if it fulfills the condition """
+        if self.filter(sample):
+            self.send_to_queue(sample)
+
+    def send_to_queue(self, sample):
+        """ TODO """
+        pass
+
+
+class RelationRepository(Singleton):
+    def __init__(self):
+        self.relations = []
+
+    def add_relation(self, filter, queue, worker):
+        self.relations.append(filter, queue, worker)
+
+    def submit_sample(self, sample):
+        for relation in self.relations:
+            relation.send_to_queue_if_necessary(sample)
