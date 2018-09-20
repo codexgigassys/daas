7c373263b4e1b827fd03501c1f483a23ce89e670
lucasesposito <espositolucas95@gmail.com>
2018-09-20 20:45:35 -0300

    django urls modified

---
+++ daas/daas_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import re_path
+from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+    re_path(r'^upload_file/?$', views.upload_file, name='upload_file')
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
