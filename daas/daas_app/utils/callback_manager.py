1223c8840c80e8f711dfb5fa1939aececeb3107d
lucasesposito <espositolucas95@gmail.com>
2018-12-13 16:36:46 -0300

    added a different lock for callback manager internal sync methods

--- daas/daas_app/utils/callback_manager.py
+++ daas/daas_app/utils/callback_manager.py
@@ -1,19 +1,23 @@
 import requests
+import threading
 
 from .singleton import ThreadSafeSingleton, synchronized
 from ..models import Sample
 from ..serializers import ResultSerializer
 
 
+lock = threading.Lock()
+
+
 class CallbackManager(metaclass=ThreadSafeSingleton):
     def __init__(self):
         self.urls = {}
 
-    @synchronized
+    @synchronized(lock)
     def add_url(self, url, sha1):
         self.urls[sha1] = [url] if sha1 not in self.urls else list(set(self.urls[sha1] + [url]))
 
-    @synchronized
+    @synchronized(lock)
     def call_from_sha1(self, sha1):
         urls = self.urls.pop(sha1)
         for url in urls:
