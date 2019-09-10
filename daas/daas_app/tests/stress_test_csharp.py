364209039ccb95f9945d8aa561118bb6e10af1d3
lucasesposito <espositolucas95@gmail.com>
2019-09-10 15:14:46 -0300

    use latest versions of docker and docker compose on circle ci

--- daas/daas_app/tests/stress_test_csharp.py
+++ daas/daas_app/tests/stress_test_csharp.py
@@ -14,14 +14,14 @@ class CsharpTest(CustomLiveServerTestCase):
         samples = Sample.objects.all().reverse()
         # Wait until all samples are finished
         for sample in samples:
-            sleep_seconds = 120
+            retries = 0
             sample = Sample.objects.get(sha1=sample.sha1)  # reload
             while not sample.finished():
                 logging.info('Sleeping %s seconds, because sample #%s (sha1: %s) status is %s'
-                             % (sleep_seconds, sample.id, sample.sha1, sample.status()))
-                time.sleep(sleep_seconds)
-                sleep_seconds = int(sleep_seconds*0.75) - 1
-                if sleep_seconds <= 0:
+                             % (5, sample.id, sample.sha1, sample.status()))
+                time.sleep(5)
+                retries += 1
+                if retries > 10:
                     assert False
                 sample = Sample.objects.get(sha1=sample.sha1)  # reload
             logging.info('Finished processing sample #%s (sha1: %s)! Status: %s'
