0fb1148947fa0fabc2da93c98606bd13cee18dc4
Lucas <espositolucas95@gmail.com>
2019-09-30 17:55:14 -0300

    starting to use echart library, samples per size chart working

---
+++ daas/daas_app/utils/chart_test.py
@@ -0,0 +1,27 @@
+from pyecharts.faker import Faker
+from pyecharts import options as opts
+from pyecharts.charts import Bar
+from daas_app.utils.statistics_manager import StatisticsManager
+from daas_app.utils.configuration_manager import ConfigurationManager
+from pyecharts import options as opts
+
+
+def bar_stack0() -> Bar:
+    chart = Bar().set_series_opts(label_opts=opts.LabelOpts(is_show=False))
+    chart.set_global_opts(title_opts=opts.TitleOpts(title="Samples per size"))
+    chart.set_global_opts(yaxis_opts=opts.AxisOpts(interval=1))
+    chart.set_global_opts(xaxis_opts=opts.AxisOpts(interval=1))
+
+    # Data (y axis)
+    range_groups = [StatisticsManager().get_sizes_for_file_type(file_type, range_length=50) for file_type in ConfigurationManager().get_identifiers()]
+    for range_group in range_groups:
+        # Add values on the yaxis for each file type.
+        # If the count is 0, replace it by None to avoid overlapped numbers in the chart.
+        chart.add_yaxis(range_group.file_type, [count if count > 0 else None for count in range_group.counts], stack="stack1")
+
+    # Captions (x axis)
+    chart.add_xaxis(range_groups[0].captions)
+    return chart
+
+
+bar_stack0().render()
--- daas/daas_app/utils/statistics_manager.py
+++ daas/daas_app/utils/statistics_manager.py
@@ -1,13 +1,26 @@
 from django_redis import get_redis_connection
-from typing import SupportsBytes, List, Tuple
+from typing import SupportsBytes, List, Tuple, Dict
+import itertools
 
+from daas_app.utils.configuration_manager import ConfigurationManager
 from .singleton import ThreadSafeSingleton
 
 
 class StatisticsManager(metaclass=ThreadSafeSingleton):
     def __init__(self) -> None:
         self.redis = get_redis_connection("default")
-        self.fields = ['result.status', 'size', 'uploaded_on', 'result.processed_on']
+
+    def _get_statistics_for(self, file_type: str, field: str):
+        return self.redis.hgetall(f'{file_type}:{field}')
+
+    def _get_maximum_for_field(self, field):
+        values_per_file_type = [self._get_statistics_for(file_type, field).keys() for file_type in ConfigurationManager().get_identifiers()]
+        flattened_values = [int(value) for value in list(itertools.chain.from_iterable(values_per_file_type))]
+        return max(flattened_values)
+
+    def get_sizes_for_file_type(self, file_type, range_length=50):
+        statistics = self._get_statistics_for(file_type=file_type, field='size')
+        return RangeGroup(file_type=file_type, statistics=statistics, maximum=self._get_maximum_for_field('size'), range_length=range_length)
 
     def report_uploaded_sample(self, sample) -> None:
         """ Use this method after receiving a new sample. If the sample is not new, you should not use this method. """
@@ -57,3 +70,56 @@ class StatisticsManager(metaclass=ThreadSafeSingleton):
     def _get_elapsed_time(self, sample) -> Tuple[str, str]:
         """ returns elapsed time in seconds. """
         return 'elapsed_time', str(sample.result.elapsed_time)
+
+
+class Range:
+    def __init__(self, minimum, range_length):
+        self.minimum = minimum
+        self.range_length = range_length
+        self.count = 0
+
+    @property
+    def maximum(self) -> int:
+        return self.minimum + self.range_length - 1
+
+    def value_in_range(self, value: int) -> bool:
+        return self.minimum <= value <= self.maximum
+
+    def increment_count_by(self, count: int) -> None:
+        self.count += count
+
+    @property
+    def caption(self) -> str:
+        return f'{self.minimum} - {self.maximum}'
+
+
+class RangeGroup:
+    def __init__(self, file_type: str, statistics: Dict[bytes, bytes], maximum: int, range_length: int):
+        self.ranges = self._generate_ranges(0, maximum, range_length)
+        self.file_type = file_type
+        self._load_statistics(statistics)
+
+    def _load_statistics(self, statistics: Dict[bytes, bytes]) -> None:
+        for value, count in statistics.items():
+            self._add_count_to_corresponding_range(int(value), int(count))
+
+    def _generate_ranges(self, minimum: int, maximum: int, range_length: int) -> List[Range]:
+        latest_range = Range(minimum, range_length)
+        if latest_range.maximum <= maximum:
+            return [latest_range] + self._generate_ranges(latest_range.maximum + 1, maximum, range_length)
+        else:
+            return [latest_range]
+
+    def _add_count_to_corresponding_range(self, value: int, count: int) -> None:
+        for range in self.ranges:
+            if range.value_in_range(value):
+                range.increment_count_by(count)
+
+    @property
+    def captions(self) -> List[str]:
+        return [range.caption for range in self.ranges]
+
+    @property
+    def counts(self) -> List[int]:
+        return [range.count if range.count > 0 else None for range in self.ranges]
+
