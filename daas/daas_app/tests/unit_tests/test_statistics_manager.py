019149a3a73fe53eed91686b11b19ef71b665606
Lucas <espositolucas95@gmail.com>
2019-10-05 23:21:06 -0300

    added tests for statistics related to file type keys

--- daas/daas_app/tests/unit_tests/test_statistics_manager.py
+++ daas/daas_app/tests/unit_tests/test_statistics_manager.py
@@ -1,7 +1,7 @@
-import time
 import datetime
 import string
 import random
+from typing import Dict
 
 from ..test_utils.test_cases.generic import APITestCase
 from ...utils.redis_status import FAILED, PROCESSING, QUEUED, DONE, CANCELLED
@@ -44,8 +44,11 @@ class AbstractStatisticsTestCase(APITestCase):
             RedisJob.objects.create(sample=sample, job_id=str(i), status=redis_job_status)
             Result.objects.create(elapsed_time=elapsed_time, status=result_status, decompiler='', sample=sample, output='')
 
-    def _get_value_from_redis(self, file_type, field):
+    def _get_statistics_from_redis(self, file_type: str, field: str) -> Dict[bytes, bytes]:
         return self.statistics_manager._redis.get_statistics_for(file_type, field)
+    
+    def _get_value_from_redis(self, file_type: str) -> str:
+        return self.statistics_manager._redis.get_count_for_file_type(file_type)
 
 
 class SameSampleStatisticsTest(AbstractStatisticsTestCase):
@@ -54,20 +57,23 @@ class SameSampleStatisticsTest(AbstractStatisticsTestCase):
         self._create_samples_with_result(file_type='flash', size=14, amount=7, redis_job_status=redis_status.DONE,
                                          result_status=result_status.SUCCESS, elapsed_time=13)
 
+    def test_file_type_statistics(self):
+        self.assertEquals(self._get_value_from_redis('flash'), 7)
+
     def test_size_statistics(self):
-        self.assertDictEqual(self._get_value_from_redis('flash', 'size'), {b'14': b'7'})
+        self.assertDictEqual(self._get_statistics_from_redis('flash', 'size'), {b'14': b'7'})
 
     def test_uploaded_on_statistics(self):
-        self.assertDictEqual(self._get_value_from_redis('flash', 'uploaded_on'), {self.today: b'7'})
+        self.assertDictEqual(self._get_statistics_from_redis('flash', 'uploaded_on'), {self.today: b'7'})
 
     def test_processed_on_statistics(self):
-        self.assertDictEqual(self._get_value_from_redis('flash', 'processed_on'), {self.today: b'7'})
+        self.assertDictEqual(self._get_statistics_from_redis('flash', 'processed_on'), {self.today: b'7'})
 
     def test_elapsed_time_statistics(self):
-        self.assertDictEqual(self._get_value_from_redis('flash', 'elapsed_time'), {b'13': b'7'})
+        self.assertDictEqual(self._get_statistics_from_redis('flash', 'elapsed_time'), {b'13': b'7'})
 
     def test_status_statistics(self):
-        self.assertDictEqual(self._get_value_from_redis('flash', 'status'), {b'0': b'7'})
+        self.assertDictEqual(self._get_statistics_from_redis('flash', 'status'), {b'0': b'7'})
 
 
 class DifferentSamplesStatisticsTest(AbstractStatisticsTestCase):
@@ -80,21 +86,24 @@ class DifferentSamplesStatisticsTest(AbstractStatisticsTestCase):
         self._create_samples_with_result(file_type='pe', size=110, amount=3, redis_job_status=redis_status.DONE,
                                          result_status=result_status.SUCCESS, elapsed_time=5)
 
+    def test_file_type_statistics(self):
+        self.assertEquals(self._get_value_from_redis('pe'), 5 + 7 + 3)
+
     def test_size_statistics(self):
-        self.assertDictEqual(self._get_value_from_redis('pe', 'size'), {b'10': b'5', b'20': b'7', b'110': b'3'})
+        self.assertDictEqual(self._get_statistics_from_redis('pe', 'size'), {b'10': b'5', b'20': b'7', b'110': b'3'})
 
     def test_uploaded_on_statistics(self):
-        self.assertDictEqual(self._get_value_from_redis('pe', 'uploaded_on'), {self.today: bytes(str(5 + 7 + 3).encode('utf-8'))})
+        self.assertDictEqual(self._get_statistics_from_redis('pe', 'uploaded_on'), {self.today: bytes(str(5 + 7 + 3).encode('utf-8'))})
 
     def test_processed_on_statistics(self):
-        self.assertDictEqual(self._get_value_from_redis('pe', 'processed_on'), {self.today: bytes(str(5 + 7 + 3).encode('utf-8'))})
+        self.assertDictEqual(self._get_statistics_from_redis('pe', 'processed_on'), {self.today: bytes(str(5 + 7 + 3).encode('utf-8'))})
 
     def test_elapsed_time_statistics(self):
-        self.assertDictEqual(self._get_value_from_redis('pe', 'elapsed_time'), {b'5': bytes(str(7 + 3).encode('utf-8')),
+        self.assertDictEqual(self._get_statistics_from_redis('pe', 'elapsed_time'), {b'5': bytes(str(7 + 3).encode('utf-8')),
                                                                                 b'13': b'5'})
 
     def test_status_statistics(self):
-        self.assertDictEqual(self._get_value_from_redis('pe', 'status'), {b'0': bytes(str(5 + 3).encode('utf-8')),
+        self.assertDictEqual(self._get_statistics_from_redis('pe', 'status'), {b'0': bytes(str(5 + 3).encode('utf-8')),
                                                                           b'2': b'7'})
 
 
@@ -107,17 +116,20 @@ class DeletedResultRevertsSomeStatisticsTest(AbstractStatisticsTestCase):
                                          result_status=result_status.SUCCESS, elapsed_time=13)
         Sample.objects.last().delete()
 
+    def test_file_type_statistics_not_affected(self):
+        self.assertEquals(self._get_value_from_redis('flash'), 7)
+
     def test_size_statistics_not_affected(self):
-        self.assertDictEqual(self._get_value_from_redis('flash', 'size'), {b'14': b'7'})
+        self.assertDictEqual(self._get_statistics_from_redis('flash', 'size'), {b'14': b'7'})
 
     def test_uploaded_on_statistics_not_affected(self):
-        self.assertDictEqual(self._get_value_from_redis('flash', 'uploaded_on'), {self.today: b'7'})
+        self.assertDictEqual(self._get_statistics_from_redis('flash', 'uploaded_on'), {self.today: b'7'})
 
     def test_processed_on_statistics_not_affected(self):
-        self.assertDictEqual(self._get_value_from_redis('flash', 'processed_on'), {self.today: b'7'})
+        self.assertDictEqual(self._get_statistics_from_redis('flash', 'processed_on'), {self.today: b'7'})
 
     def test_elapsed_time_statistics_reduced_by_one(self):
-        self.assertDictEqual(self._get_value_from_redis('flash', 'elapsed_time'), {b'13': b'6'})
+        self.assertDictEqual(self._get_statistics_from_redis('flash', 'elapsed_time'), {b'13': b'6'})
 
     def test_status_statistics_reduced_by_one(self):
-        self.assertDictEqual(self._get_value_from_redis('flash', 'status'), {b'0': b'6'})
+        self.assertDictEqual(self._get_statistics_from_redis('flash', 'status'), {b'0': b'6'})
