02d51c3b1df614fd13e89a894bd16fccb3d91608
lucasesposito <espositolucas95@gmail.com>
2019-10-09 14:52:23 -0300

    dynamic port allocation on testing

--- daas/daas_app/utils/connections/django_server.py
+++ daas/daas_app/utils/connections/django_server.py
@@ -8,9 +8,26 @@ class DjangoServerConfiguration:
     def ip(self):
         return socket.gethostbyname(socket.gethostname())
 
+    def _get_port_for_test_instance(self, increment: int = 0):
+        port = settings.TEST_INSTANCE_PORT + increment
+        return port if self._is_available(port) else self._get_port_for_test_instance(increment + 1)
+
+    def _is_available(self, port):
+        """ Returns whether a port is available or not. """
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            try:
+                sock.bind(("0.0.0.0", port))
+            except:
+                available = False
+            else:
+                available = True
+        return available
+
     @property
     def port(self):
-        return settings.TEST_INSTANCE_PORT if ('test' in sys.argv) else settings.DEFAULT_PORT
+        """ Returns the default port. If running tests, returns the first available port starting from the
+            default test port defined on settings.py. """
+        return self._get_port_for_test_instance() if ('test' in sys.argv) else settings.DEFAULT_PORT
 
     @property
     def base_url(self):
