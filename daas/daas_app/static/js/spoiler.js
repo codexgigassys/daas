feb9d3c4b83a410d8f54702fc561a321249b511b
lucasesposito <espositolucas95@gmail.com>
2019-10-25 17:58:30 -0300

    navbar fixed at top, simplified search and moved common fields outside spoiler

--- daas/daas_app/static/js/spoiler.js
+++ daas/daas_app/static/js/spoiler.js
@@ -4,11 +4,11 @@ $(function(){
 		$(this).next().animate({height: 'toggle'});
 		if ($('.spoiler-text').hasClass("do_not_show")){
 			$('.spoiler-text').removeClass("do_not_show");
-			$(this).html('<a id="search-text" href="#search"><span class="glyphicon glyphicon-plus"></span>&nbsp;Search</a>');
+			$(this).html('<a id="search-text" href="#search"><span class="glyphicon glyphicon-plus"></span>&#x25BC;Advanced Search</a>');
 		}
 		else{
 			$('.spoiler-text').addClass("do_not_show");
-			$(this).html('<a id="search-text" href="#search"><span class="glyphicon glyphicon-minus"></span>&nbsp;Search</a>');
+			$(this).html('<a id="search-text" href="#search"><span class="glyphicon glyphicon-minus"></span>&#x25BC;Advanced Search</a>');
 		}
 	}); // end spoiler-toggle
 }); // end document ready
