6e65354f87d630cccbda889486d9be1c19c51299
Lucas <espositolucas95@gmail.com>
2019-10-28 17:20:10 -0300

    removed unused code

--- daas/daas_app/api.py
+++ daas/daas_app/api.py
@@ -2,24 +2,17 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 import hashlib
 from rest_framework.parsers import MultiPartParser
-from rest_framework.parsers import JSONParser
-from rest_framework.parsers import MultiPartParser, FormParser, FileUploadParser
 import logging
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from django.contrib.auth import authenticate
-from rest_framework.status import (
-    HTTP_400_BAD_REQUEST,
-    HTTP_404_NOT_FOUND,
-    HTTP_200_OK
-)
 from rest_framework import status
 
 from .models import Sample
 from .utils.reprocess import reprocess
-from .serializers import SampleWithoutDataSerializer, SampleSerializer, ResultSerializer
+from .serializers import SampleSerializer
 from .utils.upload_file import upload_file
 from .utils.callback_manager import CallbackManager
 from .utils.classifier import ClassifierError
@@ -32,19 +25,17 @@ def get_token(request):
     username = request.POST['username']
     password = request.POST['password']
     if username is None or password is None:
-        return Response({'error': 'Please provide both username and password'},
-                        status=HTTP_400_BAD_REQUEST)
+        return Response({'message': 'Please provide both username and password'},
+                        status=status.HTTP_400_BAD_REQUEST)
     user = authenticate(username=username, password=password)
     if not user:
-        return Response({'error': 'Invalid Credentials'},
-                        status=HTTP_404_NOT_FOUND)
+        return Response({'message': 'Invalid Credentials'},
+                        status=status.HTTP_404_NOT_FOUND)
     token, _ = Token.objects.get_or_create(user=user)
     return Response({'token': token.key}, status=status.HTTP_200_OK)
 
 
 class GetSampleFromHashAPIView(APIView):
-    parser_classes = (JSONParser,)
-
     def get(self, request, hash):
         try:
             sample = Sample.objects.get_sample_with_hash(hash)
@@ -77,7 +68,6 @@ class UploadAPIView(APIView):
                     CallbackManager().add_url(callback, hashlib.sha1(content).hexdigest())
                 else:
                     CallbackManager().call(callback, hashlib.sha1(content).hexdigest())
-        logging.info('File uploaded. Returning status 202.')
         return Response(status=status.HTTP_202_ACCEPTED)
 
 
@@ -88,6 +78,7 @@ class ReprocessAPIView(APIView):
         callback = request.data.get('callback', None)
         logging.info(f'Reprocess API. Force reprocess: {force_reprocess}. Callback: {callback}. Hashes: {hashes}')
         samples = Sample.objects.with_hash_in(hashes)
+
         if not force_reprocess:
             # Return data for samples processed with the latest decompiler.
             for sample in samples.processed_with_current_decompiler_version():
@@ -99,5 +90,4 @@ class ReprocessAPIView(APIView):
             CallbackManager().add_url(callback, sample.sha1)
             reprocess(sample, force_reprocess=force_reprocess)
 
-        logging.info('Files sent for reprocess (if needed or forced). Returning status 202.')
         return Response(status=status.HTTP_202_ACCEPTED)
