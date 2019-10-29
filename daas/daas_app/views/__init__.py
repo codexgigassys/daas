b53404411190b0ed80ad681eadca1a2039794a84
lucasesposito <espositolucas95@gmail.com>
2019-10-29 16:29:21 -0300

    fixed missing import, linked api docs into navbar

--- daas/daas_app/views/__init__.py
+++ daas/daas_app/views/__init__.py
@@ -1,5 +1,3 @@
 from .charts import (SamplesPerType, SamplesPerSize, SamplesPerElapsedTime, SamplesPerUploadDate, SamplesPerProcessDate,
                      SamplesPerStatusForFileType)
-from .chart_data import (SamplesPerTypeData, SamplesPerElapsedTimeData, SamplesPerSizeData, SamplesPerUploadDateData,
-                         SamplesPerProcessDateData, SamplesPerStatusForFileTypeData)
 from .other_views import *
