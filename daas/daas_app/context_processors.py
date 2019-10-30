f43277e98821af29f165db9dc75492b92f2b227c
lucasesposito <espositolucas95@gmail.com>
2019-10-30 16:48:07 -0300

    use redis instead of the DB to count samples, minor changes on navbar

--- daas/daas_app/context_processors.py
+++ daas/daas_app/context_processors.py
@@ -1,14 +1,12 @@
 from .models import Sample
-
+from .utils.charts.statistics_manager import StatisticsManager
 # Increase this number after every release to force browsers to download the latest static files
 RELEASE_NUMBER = 10
 
 
 def release_number(request):
-    return {'release_number': "?v=%s" % RELEASE_NUMBER}
+    return {'release_number': f'?v={RELEASE_NUMBER}'}
 
 
 def sample_count_context(request):
-    context_data = dict()
-    context_data['samples_count'] = Sample.objects.finished().count()  # fixme: replace using redis!
-    return context_data
+    return {'samples_count': StatisticsManager().get_total_sample_count()}
