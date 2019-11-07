d18be7e209717bdf5d2dc18a666d5e367831cb9c
lucasesposito <espositolucas95@gmail.com>
2019-11-07 10:36:37 -0300

    use java archives to test java

--- daas/daas_app/tests/decompilation_ratio_tests/test_java_decompilation_ratio.py
+++ daas/daas_app/tests/decompilation_ratio_tests/test_java_decompilation_ratio.py
@@ -1,13 +1,13 @@
 from ..test_utils.test_cases.generic import NonTransactionalLiveServerTestCase
 from ..test_utils.test_cases.decompilation_ratio import DecompilationRatioTestCaseMixin
-from ..test_utils.resource_directories import CSHARP_ZIPPED_PACK
+from ..test_utils.resource_directories import JAVA_ZIPPED_PACK
 
 
 class JavaTest(NonTransactionalLiveServerTestCase, DecompilationRatioTestCaseMixin):
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
-        super().postSetUpClass(zipped_samples_path=CSHARP_ZIPPED_PACK,
+        super().postSetUpClass(zipped_samples_path=JAVA_ZIPPED_PACK,
                                timeout_per_sample=1200,
                                decompiled_samples=84,
                                failed_samples=0,
