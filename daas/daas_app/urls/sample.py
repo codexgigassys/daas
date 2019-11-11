a7b49d703c1b7d358cd8a4c2bd9f0097922ce530
lucasesposito <espositolucas95@gmail.com>
2019-11-11 11:47:51 -0300

    fixed error cancelling tasks

--- daas/daas_app/urls/sample.py
+++ daas/daas_app/urls/sample.py
@@ -10,5 +10,5 @@ sample_urlpatterns = [
     re_path(r'^reprocess/(?P<sample_id>[0-9]+)/?$', views.reprocess_view, name='reprocess'),
     re_path(r'^download_sample/(?P<sample_id>[0-9]+)/?$', api.download_sample_view, name='download_sample'),
     re_path(r'^delete_sample/(?P<pk>[0-9]+)/?$', views.SampleDeleteView.as_view(), name='delete_sample'),
-    re_path(r'^cancel_job/(?P<redis_job_pk>[0-9]+)/?$', views.cancel_job_view, name='cancel_job')
+    re_path(r'^cancel_task/(?P<pk>[0-9]+)/?$', views.cancel_job_view, name='cancel_task')
 ]
