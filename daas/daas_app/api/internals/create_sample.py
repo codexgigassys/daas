bc3d1b0e8818d8d94680730f9d51cf3b1d6beebd
Lucas <espositolucas95@gmail.com>
2020-01-20 14:32:23 -0300

    push samples to queue after creation, sample pushing to queue is now thread safe

--- daas/daas_app/api/internals/create_sample.py
+++ daas/daas_app/api/internals/create_sample.py
@@ -6,10 +6,14 @@ from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 import requests
 import logging
+from typing import List
+from rest_framework.request import Request
 
 from ...utils.callback_manager import CallbackManager
+from ...utils.task_manager import TaskManager
 from ...serializers import SampleSerializer
 from ...utils.lists import recursive_flatten
+from ...models import Sample
 
 
 class CreateSampleView(APIView):
@@ -36,21 +40,24 @@ class CreateSampleView(APIView):
             )
         }
     )
-    def post(self, request):
+    def post(self, request: Request) -> Response:
         force_reprocess = request.data.get('force_reprocess', False)
         callback = request.data.get('callback')
         sample_data = request.data.get('sample')
-        samples = self.serialize_sample(sample_data)
+        samples = self._serialize_sample(sample_data)
+
+        self._submit_samples(samples)
+
 
         if callback:
             pass  # todo do callback magic here
         return Response(status=status.HTTP_200_OK, data={'non_zip_samples': len(samples)})
 
-    def serialize_sample(self, sample_data):
+    def _serialize_sample(self, sample_data: dict) -> List[Sample]:
         if not sample_data:  # No sample to serialize (either sample not found by meta_extractor or subfiles of an empty zip)
             samples = []
         elif sample_data['file_type'] == 'zip':  # Zip sample
-            samples = [self.serialize_sample(subfile) for subfile in sample_data['subfiles']]
+            samples = [self._serialize_sample(subfile) for subfile in sample_data['subfiles']]
         else:  # Non-zip sample
             sample_serializer = SampleSerializer(data=sample_data)
             if sample_serializer.is_valid():
@@ -59,3 +66,7 @@ class CreateSampleView(APIView):
             else:
                 logging.info(f'Create sample: serializer is not valid: {sample_serializer.data=}')
         return recursive_flatten(samples)
+
+    def _submit_samples(self, samples: List[Sample]) -> None:
+        for sample in samples:
+            TaskManager().submit_sample(sample)
--- daas/daas_app/utils/task_manager.py
+++ daas/daas_app/utils/task_manager.py
@@ -5,16 +5,23 @@ from rq.job import Job
 from redis import Redis
 from typing import Tuple, Optional
 from datetime import date
+import logging
+from threading import Lock
+from django.db import transaction
 
 from .connections.django_server import DjangoServerConfiguration
 from .singleton import ThreadSafeSingleton
 from .configuration_manager import ConfigurationManager
 from ..tests.mocks.task import MockTask
 from ..models import Sample
+from .status.sample import SampleStatus
+
+
+_task_lock = Lock()
 
 
 class TaskManager(metaclass=ThreadSafeSingleton):
-    def __init__(self):
+    def __init__(self) -> None:
         self.connection = Redis(host='daas_redis_task_queue_1')
         # Where to look for decompilers' code
         self.worker_path = 'daas.worker.worker'
@@ -36,16 +43,31 @@ class TaskManager(metaclass=ThreadSafeSingleton):
     def get_task(self, identifier: str, task_id: int) -> Job:
         return self.get_queue(identifier).fetch_job(task_id)
 
-    def submit_sample(self, sample: Sample) -> Tuple[str, int]:
-        configuration = ConfigurationManager().get_config_for_sample(sample)
-        queue = self.get_queue(configuration.identifier)
-        task = queue.enqueue(self.worker_path,
-                             args=({'sample_id': sample.id,
-                                    'config': configuration.as_dictionary(),
-                                    'api_base_url': DjangoServerConfiguration().base_url},))
-        return configuration.identifier, task.id
+    def needs_processing(self, sample: Sample, force_process: bool = False) -> bool:
+        non_intermediate_status = sample.status not in [SampleStatus.QUEUED, SampleStatus.PROCESSING]
+        return (sample.requires_processing or force_process) and non_intermediate_status
 
-    def cancel_task(self, identifier, task_id) -> None:
+    def submit_sample(self, sample: Sample, force_process: bool = False) -> None:
+        # Use locks to not push the same file two times. After the first push, the second one will return
+        # self.needs_processing() => False, and the second copy of the file will not be pushed to the queue.
+        _task_lock.acquire()
+        if self.needs_processing(sample, force_process):
+            configuration = ConfigurationManager().get_config_for_sample(sample)
+            queue = self.get_queue(configuration.identifier)
+            task_id = queue.enqueue(self.worker_path,
+                                    args=({'sample_id': sample.id,
+                                           'config': configuration.as_dictionary(),
+                                           'api_base_url': DjangoServerConfiguration().base_url},))
+            with transaction.atomic():
+                sample.wipe()  # for reprocessing or non-finished processing.
+                from ..models import Task  # To avoid circular imports
+                Task.objects.create(task_id=task_id, sample=sample)  # assign the new task to the sample
+            logging.info(f'File {sample.sha1=} sent to the queue with {task_id=}')
+        else:
+            logging.info(f'Processing was requested for file {sample.sha1=} but it is unneeded.')
+        _task_lock.release()
+
+    def cancel_task(self, identifier: str, task_id: int) -> None:
         if task_id is not None:
             task = self.get_task(identifier, task_id)
             if task is not None:
@@ -78,6 +100,6 @@ class TaskManager(metaclass=ThreadSafeSingleton):
     def __mock_calls_submit_sample__(self) -> int:
         return self.__mock_calls_submit_sample
 
-    def __submit_sample_mock__(self, sample) -> Tuple[str, int]:
+    def __submit_sample_mock__(self, sample: Sample) -> Tuple[str, int]:
         self.__mock_calls_submit_sample += 1
         return ConfigurationManager().get_config_for_sample(sample).identifier, self.__mock_task.id
