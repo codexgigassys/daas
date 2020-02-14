79d52d08771859f28373b478a438cfb6d2429826
lucasesposito <espositolucas95@gmail.com>
2020-02-14 15:25:42 -0300

    fix

--- daas/daas_app/utils/callback_manager/callback_manager.py
+++ daas/daas_app/utils/callback_manager/callback_manager.py
@@ -16,17 +16,17 @@ class CallbackManager(metaclass=ThreadSafeSingleton):
         self._redis = CallbacksRedis()
 
     @synchronized(lock)
-    def add_url(self, sample_sha1: str, callback_url: SupportsBytes) -> None:
-        self._redis.add_callback(sample_sha1, callback_url)
+    def add_url(self, sample_sha1: str, callback_url: Optional[SupportsBytes]) -> None:
+        if callback_url:
+            self._redis.add_callback(sample_sha1, callback_url)
 
     @synchronized(lock)
     def send_callbacks(self, sample_sha1: str) -> None:
         for callback_url in self._redis.get_callbacks(sample_sha1):
             self._call(sample_sha1, callback_url)
 
-    def _call(self, sample_sha1: str, callback_url: Optional[SupportsBytes]) -> None:
-        if callback_url:
-            requests.post(callback_url, SampleSerializer(Sample.objects.get(sha1=sample_sha1)).data)
+    def _call(self, sample_sha1: str, callback_url: SupportsBytes) -> None:
+        requests.post(callback_url, SampleSerializer(Sample.objects.get(sha1=sample_sha1)).data)
 
     """ test methods: """
     def __mock__(self) -> None:
