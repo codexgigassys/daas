6291cd0d95926e287f09eb668233e571b68ba059
CrimsonGlory <javierbassi@gmail.com>
2024-07-23 16:40:10 +0000

    ignore these pep8 rules

--- daas/daas_app/tests/unit_tests/test_pep8.py
+++ daas/daas_app/tests/unit_tests/test_pep8.py
@@ -7,6 +7,6 @@ class TestPEP8(TestCase):
         output = subprocess.check_output(
             ["find", "/daas/", "-type", "f", "-name",
              "*.py", "-exec", "pycodestyle", "--max-line-length=34000",
-             "--ignore=E121,E123,E126,E226,E24,E704,W503,W605,E741,E722",
+             "--ignore=E121,E123,E126,E203,E226,E231,E24,E704,W503,W605,E741,E701,E722",
              "{}", ";"])
         assert len(output.strip()) == 0, 'Should be zero. (PEP8 test failed)\n' + str(output.strip())
