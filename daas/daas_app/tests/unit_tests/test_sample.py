2148c15c7abba75bc0d06a8f690765fc9d3ef6d1
CrimsonGlory <javierbassi@gmail.com>
2025-01-17 02:04:59 +0000

    more testing to sample.py

--- daas/daas_app/tests/unit_tests/test_sample.py
+++ daas/daas_app/tests/unit_tests/test_sample.py
@@ -4,6 +4,35 @@ from daas_app.models.sample import SampleQuerySet
 
 
 class TestSample(TestCase):
+    def test_sample(self):
+        # create Sample with random content (asdf)
+        w = Sample(md5="912ec803b2ce49e4a541068d495ab570",
+                   sha1="3da541559918a808c2402bba5012f6c60b27661c",
+                   sha2="f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b",
+                   size=4, file_name="test.txt", file_type='txt')
+        w.save()
+        # Source code is no available because it is not saved in weedfs
+        assert w.source_code is None
+        assert w.finished() is False
+        assert w.unfinished() is True
+        assert w.downloadable() is True
+        assert w.is_possible_to_reprocess() is False
+
+    def test_sample_query_set_with_id_in(self):
+        # Test SampleQuerySet: with_id_in
+        tmp_sample_query_set = Sample.objects.with_id_in([1, 2])
+        assert isinstance(tmp_sample_query_set, SampleQuerySet)
+        assert tmp_sample_query_set.count() == 0
+
+    def test_sample_query_set_decompiler_version(self):
+        # Test SampleQuerySet: processed_with_old_decompiler_version
+        tmp_sample_query_set = Sample.objects.processed_with_old_decompiler_version()
+        assert isinstance(tmp_sample_query_set, SampleQuerySet)
+        assert tmp_sample_query_set.count() == 0
+        tmp_sample_query_set = Sample.objects.processed_with_current_decompiler_version()
+        assert isinstance(tmp_sample_query_set, SampleQuerySet)
+        assert tmp_sample_query_set.count() == 0
+
     def test_sample_query_set_with_size_between(self):
         # Test SampleQuerySet: with_size_between
         tmp_sample_query_set = Sample.objects.with_size_between(0, 2)
