c446301717f8707abd9cb36fe7d1bf5dbe40ece3
CrimsonGlory <CrimsonGlory@users.noreply.github.com>
2025-03-19 18:38:04 -0300

    DAAS-196 upgrade to django 3.0 (#65)

--- daas/daas_app/admin.py
+++ daas/daas_app/admin.py
@@ -5,18 +5,16 @@ from daas_app.models.result import Result
 
 
 # Register your models here.
+@admin.register(Sample)
 class SampleAdmin(admin.ModelAdmin):
     list_display = ('id', 'sha1', 'file_name', 'size', 'seaweedfs_file_id', 'uploaded_on')
 
 
+@admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('id', 'task_id', '_status', 'created_on', 'sample')
 
 
+@admin.register(Result)
 class ResultAdmin(admin.ModelAdmin):
     list_display = ('id', 'timeout', 'elapsed_time', 'exit_status', 'status', 'output', 'seaweed_result_id', 'decompiler', 'sample', 'processed_on', 'version', 'extension')
-
-
-admin.site.register(Sample, SampleAdmin)
-admin.site.register(Task, TaskAdmin)
-admin.site.register(Result, ResultAdmin)
